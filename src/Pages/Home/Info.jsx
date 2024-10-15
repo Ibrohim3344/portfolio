beda18f624431be0ddea762c5714e55df9cbca16
Ibrohim3344 <ibrohimakmaljonov43@gmail.com>
2024-10-15 17:11:43 +0500

    nwee

--- src/Pages/Home/Info.jsx
+++ src/Pages/Home/Info.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { TypeAnimation } from 'react-type-animation';
+import me from "/public/mee.png"
 
 const Info = () => {
     const { t } = useTranslation("name");
 
-    
+
 
     return (
         <section className='bg-gray py-[50px] dark:bg-qora'>
-            <div className='container flex flex-row sm:justify-between justify-center'>
-                <div className='flex flex-col sm:items-start items-center gap-[40px]'>
+            <div className='container flex flex-row items-center lg:justify-between justify-center'>
+                <div className='flex flex-col lg:items-start items-center gap-[40px]'>
                     <h1 className='text-2xl text-qora dark:text-oq font-bold'>{t("hello")}</h1>
 
-                    <div className='flex flex-col sm:items-start items-center gap-[10px]'>
+                    <div className='flex flex-col lg:items-start items-center gap-[10px]'>
 
                         {/* md dan kichik ekranlar uchun animatsiya */}
-                        <div className="hidden md:flex">
+                        <div className="hidden lg:flex">
                             <TypeAnimation
                                 className='text-5xl lg:text-5xl xl:text-6xl text-qora dark:text-oq font-semibold'
                                 key={localStorage.getItem("language")}
@@ -40,11 +41,14 @@ const Info = () => {
                         </div>
 
                         {/* md dan katta ekranlar uchun statik matn */}
-                        <span className='flex md:hidden sm:text-start text-center text-5xl lg:text-5xl xl:text-6xl text-qora dark:text-oq font-semibold'>
+                        <span className='flex lg:hidden lg:text-start text-center text-5xl lg:text-5xl xl:text-6xl text-qora dark:text-oq font-semibold'>
                             {t("surname")}
                         </span>
 
                         <span className='text-blue-gray-500 sm:text-2xl text-xl'>{t("info")}</span>
+
+                        <img src={me} alt="" className='sm:w-[300px] w-[250px] object-cover sm:h-[300px] h-[250px] rounded-full lg:hidden flex' />
+
                     </div>
 
                     <div className='flex flex-row gap-[20px]'>
@@ -65,7 +69,7 @@ const Info = () => {
                 </div>
 
                 <div>
-
+                    <img src={me} alt="" width={400} className='object-cover sm:h-[400px] rounded-full lg:flex hidden' />
                 </div>
             </div>
         </section>
